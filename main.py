d133e6a0a14ed944fd1efce23bde81f059d6ec01
MidHost1 <maksim.frizorher123@gmail.com>
2025-01-03 22:11:49 +0100

    def get_fullname(first_name, last_name, middle_name = ""):     if middle_name:         return f"{first_name} {middle_name} {last_name}"     else:         return f"{first_name} {last_name}" first_name = str(input("Enter your first name: ")) last_name = str(input("Enter your last name: ")) middle_name = str(input("Enter your middle name: ")) get_fullname(first_name, last_name, middle_name)

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-print("Hello World")
-
-print("Hello Git")
-echo = input("Write here: ")
-print(echo)
+def get_fullname(first_name, last_name, middle_name = ""):
+    if middle_name:
+        return f"{first_name} {middle_name} {last_name}"
+    else:
+        return f"{first_name} {last_name}"
+first_name = str(input("Enter your first name: "))
+last_name = str(input("Enter your last name: "))
+middle_name = str(input("Enter your middle name: "))
+get_fullname(first_name, last_name, middle_name)
+    
