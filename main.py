32539a9307869fb1b32d516dae21b6d93368c586
MidHost1 <maksim.frizorher123@gmail.com>
2025-01-03 22:12:42 +0100

    fixed_problem

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ def get_fullname(first_name, last_name, middle_name = ""):
 first_name = str(input("Enter your first name: "))
 last_name = str(input("Enter your last name: "))
 middle_name = str(input("Enter your middle name: "))
-get_fullname(first_name, last_name, middle_name)
+get_fullname(first_name, last_name, middle_name)#fixed
     
